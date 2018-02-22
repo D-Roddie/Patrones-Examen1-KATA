847d37a72d2ecad95e1af0567353c0bd5d35dcf0
D-Rod <danrodret@outlook.com>
2018-02-21 19:49:03 -0600

    Se agregaron los valores de los números Romanos

--- src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
+++ src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -1,8 +1,30 @@
 package com.cenfotec.patrones.logica;
 
 public class ProcesadorRomano {
-	public String logicaRomana (String num_romano) {
-		return num_romano;
+	
+	public int valoresRomanos (char valorRomano) {
+		if(valorRomano == 'I') {
+			return 1;
+			}
+		if (valorRomano == 'V') {
+			return 5;
+			}
+		if(valorRomano == 'X') {
+			return 10;
+			}
+		if (valorRomano == 'L') {
+			return 50;
+			}
+		if(valorRomano == 'C') {
+			return 100;
+			}
+		if(valorRomano == 'D') {
+			return 500;
+		}
+		if(valorRomano == 'M') {
+			return 1000;
+		}
+		return -1;	
 	}
 
 }
