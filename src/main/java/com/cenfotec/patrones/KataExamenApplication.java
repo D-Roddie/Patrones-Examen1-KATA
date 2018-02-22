23ecdfe5a01a2c5a9821e79a0bcd23159100fc45
D-Rod <danrodret@outlook.com>
2018-02-21 19:14:19 -0600

    Creación del proyecto (Clases principales, con sus tests respectivos, en blanco)

---
+++ src/main/java/com/cenfotec/patrones/KataExamenApplication.java
@@ -0,0 +1,12 @@
+package com.cenfotec.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataExamenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cenfotec/patrones/logica/ProcesadorRomanoTest.java
@@ -0,0 +1,14 @@
+package com.cenfotec.patrones.logica;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProcesadorRomanoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
