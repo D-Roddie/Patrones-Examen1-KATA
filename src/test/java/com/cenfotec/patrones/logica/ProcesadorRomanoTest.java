d864f8e335fecc6f9076c33cd9f49528d60f13cc
D-Rod <danrodret@outlook.com>
2018-02-21 20:55:37 -0600

    Prueba fallida: El valor que se ingreso en el test no esta correcto

--- src/test/java/com/cenfotec/patrones/logica/ProcesadorRomanoTest.java
+++ src/test/java/com/cenfotec/patrones/logica/ProcesadorRomanoTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.cenfotec.patrones.KataExamenApplication;
+
 public class ProcesadorRomanoTest {
 
-
+	@Test
+	public void pruebaUno() {
+		ProcesadorRomano PR = new ProcesadorRomano();
+		assertEquals(10, PR.logicaRomanosaNumeros("LX"));
+	}
 
 }
