f90d09828f56730a5fdd0e262dab581dde9ebfe1
D-Rod <danrodret@outlook.com>
2018-02-21 20:46:10 -0600

    Refactor: El error de que lanzaba números alazar resuelto

--- src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
+++ src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -2,10 +2,30 @@ package com.cenfotec.patrones.logica;
 
 public class ProcesadorRomano {
 	
+	public int valoresRomanos (char valorRomano) {
+		if(valorRomano == 'I') {
+			return 1;}
+		if (valorRomano == 'V') {
+			return 5;}
+		if(valorRomano == 'X') {
+			return 10;}
+		if (valorRomano == 'L') {
+			return 50;}
+		if(valorRomano == 'C') {
+			return 100;}
+		if(valorRomano == 'D') {
+			return 500;}
+		if(valorRomano == 'M') {
+			return 1000;
+		}
+		return valorRomano;
+		
+	}
+	
 	public int logicaRomanosaNumeros(String num_romano) {
 		int romano = 0;
 		for (int i = 0; i < num_romano.length(); i++) {	
-			int letraUno = valoresRomanos((char) num_romano.length());
+			int letraUno = valoresRomanos(num_romano.charAt(i));
 			if (i +1 < num_romano.length()){
 				int letraDos = valoresRomanos(num_romano.charAt(i+1));
 				
@@ -22,31 +42,12 @@ public class ProcesadorRomano {
 			}
 		}
 		return romano;
-	}
+}
+
 	
-	public int valoresRomanos (char valorRomano) {
-		if(valorRomano == 'I') {
-			return 1;
-			}
-		if (valorRomano == 'V') {
-			return 5;
-			}
-		if(valorRomano == 'X') {
-			return 10;
-			}
-		if (valorRomano == 'L') {
-			return 50;
-			}
-		if(valorRomano == 'C') {
-			return 100;
-			}
-		if(valorRomano == 'D') {
-			return 500;
-		}
-		if(valorRomano == 'M') {
-			return 1000;
-		}
-		return -1;	
-	}
+	
 
 }
+		
+		
+		
